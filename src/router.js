57e4996e915d895bc2de88bbb686c5a5ad8b854b
Max Lyashuk <m_lyashuk@ukr.net>
2018-11-18 15:29:16 +0200

    perf: host `FallStack` exercise as separate webpack chunk

--- src/router.js
+++ src/router.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import DashboardPage from '@/components/pages/Dashboard.vue';
-import FallStackPage from '@/components/pages/FallStack.vue';
 import PlayPhrasesPage from '@/components/pages/PlayPhrases.vue';
+
+const FallStackPage = () => import('@/components/pages/FallStack.vue');
 
 Vue.use(Router);
 
