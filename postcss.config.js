d7589c5a9247141453c75d7e90a4fefe79af7b66
Max Liashuk <max.lyashuk@rolique.io>
2020-06-21 00:17:20 +0300

    fix: turn off purge css on dev machine

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    'vue-cli-plugin-tailwind/purgecss': {},
+    ...(process.env.NODE_ENV === 'production' ? { 'vue-cli-plugin-tailwind/purgecss': {} } : {}),
     autoprefixer: {},
   },
 };
